abf2503a224a2580dcef5d5d3a9c90ff621c36f7
Kevin Mooney <moonkev@gmail.com>
2025-03-05 16:20:16 -0600

    add startup dependencies

--- deploy/build-minikube.sh
+++ deploy/build-minikube.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env sh
 
+# Gradle Build
 cd ..
-./gradlew build
+ ./gradlew build
+
+# Docker Push
 eval $(minikube -p minikube docker-env)
-docker build -f deploy/Dockerfile -t moonkev/vertx_playground:1.0 .
+docker build -f deploy/Dockerfile -t moonkev/vertx_playground:latest .
+
+# Helm Push
+cd deploy/helm
+helm dependency update ./vertx-playground-full
+helm dependency build ./vertx-playground-full
+
+helm install \
+  --set global.vertx_playground.image.repository=moonkev/vertx_playground \
+  --set global.vertx_playground.image.tag=latest \
+  --set global.vertx_playground.image.pullPolicy=Never \
+  vertx-playground-full ./vertx-playground-full -n vertx-playground --create-namespace
